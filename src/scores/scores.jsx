7fe5a034d6cce34ea14223b317f44e8d8b5514e3
Sawyer Whipperman <sawyer57@byu.edu>
2026-02-12 11:19:23 -0700

    create view components

---
+++ src/scores/scores.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Scores() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>Scores displayed here</div>
+    </main>
+  );
+}
