abba64778f7d599b64e139004d934c11eecd8130
Sawyer Whipperman <sawyer57@byu.edu>
2026-02-12 11:36:44 -0700

    create the router

--- src/app.jsx
+++ src/app.jsx
@@ -2,32 +2,57 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Play } from './play/play';
+import { Scores } from './scores/scores';
+import { About } from './about/about';
+import { DropdownDivider } from 'react-bootstrap';
+
+
 export default function App() {
-  return <div className="body bg-dark text-light">
+  return (
+  <BrowserRouter>
+  <div className="body bg-dark text-light">
     <header className="container-fluid">
       <nav className="navbar fixed-top navbar-dark">
-        <a className="navbar-brand" href="#">Simon<sup>&reg;</sup></a>
+        <div className="navbar-brand">Simon<sup>&reg;</sup></div>
         <menu className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link active" href="index.html">Home</a>
+            <NavLink className="nav-link active" to="/">Home</NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="play.html">Play</a>
+            <NavLink className="nav-link" to="play">Play</NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="scores.html">Scores</a>
+            <NavLink className="nav-link" to="scores">Scores</NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="about.html">About</a>
+            <NavLink className="nav-link" to="about">About</NavLink>
           </li>
         </menu>
       </nav>
     </header>
-    <main className="container-fluid bg-secondary text-center">App Components Go Here </main>
+
+    <Routes>
+      <Route path='/' element={<Login />} exact />
+      <Route path='/play' element={<Play />} />
+      <Route path='/scores' element={<Scores />} />
+      <Route path='/about' element={<About />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+
     <footer className="bg-dark text-white-50">
       <div className="container-fluid">
         <span className="text-reset">Author Name(s)</span>
         <a className="text-reset" href="https://github.com/webprogramming260/simon-css">Source</a>
       </div>
-    </footer></div>;
+    </footer>
+  </div>
+  </BrowserRouter>
+  ); 
+}
+
+function NotFound() {
+  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
